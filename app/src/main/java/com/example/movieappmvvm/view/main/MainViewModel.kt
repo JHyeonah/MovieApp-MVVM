8d6a95b11a62764ec73771b4aa98976321cf3469
Hyeonah Jung <gwi02378@gmail.com>
2021-04-20 22:29:20 +0900

    add Main Viewmodel

--- app/src/main/java/com/example/movieappmvvm/view/main/MainViewModel.kt
+++ app/src/main/java/com/example/movieappmvvm/view/main/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.example.movieappmvvm.view.main
 
 import com.example.movieappmvvm.comm.BaseViewModel
 import com.example.movieappmvvm.model.Movie
+import com.example.movieappmvvm.util.Debug
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 import org.koin.core.component.KoinApiExtension
@@ -12,9 +13,13 @@ class MainViewModel(): BaseViewModel() {
         .observeOn(AndroidSchedulers.mainThread())
         .subscribeOn(Schedulers.io())
         .subscribe({
-
+            it.data.movies?.let { list ->
+                success (list)
+                Debug.log("movie list : $list")
+            }
         }, {
-
+            fail()
+            Debug.err("movie list failed : $it")
         })
     }
 }
