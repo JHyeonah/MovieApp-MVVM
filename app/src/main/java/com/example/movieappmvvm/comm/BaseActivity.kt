79a1689dfdfd08315370943b851bbe443979b45b
Hyeonah Jung <gwi02378@gmail.com>
2021-04-21 22:48:00 +0900

    MainActivity viewmodel 연결

--- app/src/main/java/com/example/movieappmvvm/comm/BaseActivity.kt
+++ app/src/main/java/com/example/movieappmvvm/comm/BaseActivity.kt
@@ -5,11 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity<R: BaseViewModel>: AppCompatActivity() {
     abstract val viewModel: R
-
+    abstract val layoutResourceId: Int
     abstract fun initView()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(layoutResourceId)
         initView()
     }
 }
--- app/src/main/java/com/example/movieappmvvm/view/main/MainActivity.kt
+++ app/src/main/java/com/example/movieappmvvm/view/main/MainActivity.kt
@@ -3,10 +3,21 @@ package com.example.movieappmvvm.view.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.movieappmvvm.R
+import com.example.movieappmvvm.comm.BaseActivity
+import com.example.movieappmvvm.util.Debug
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<MainViewModel>() {
+    override val viewModel: MainViewModel by viewModel()
+    override val layoutResourceId: Int
+        get() = R.layout.activity_main
+
+    override fun initView() {
+        viewModel.callMovies(
+            success = {
+                Debug.log("list in activity : $it")
+            },
+            fail = {}
+        )
     }
 }
