5c559904ab61be5587edfebe5e9ce6d841254d33
Hyeonah Jung <gwi02378@gmail.com>
2021-04-22 22:41:02 +0900

    타이틀바 제거, 상단바 색 변경

--- app/src/main/java/com/example/movieappmvvm/view/main/MainViewModel.kt
+++ app/src/main/java/com/example/movieappmvvm/view/main/MainViewModel.kt
@@ -7,7 +7,7 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 import org.koin.core.component.KoinApiExtension
 
-class MainViewModel(): BaseViewModel() {
+class MainViewModel: BaseViewModel() {
     fun callMovies(success: (list: ArrayList<Movie>) -> Unit, fail: () -> Unit) {
         api.getMovieList()
         .observeOn(AndroidSchedulers.mainThread())
