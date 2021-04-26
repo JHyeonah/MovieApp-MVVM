48d19308aeac1cc888938956ef1a107863c4edec
Hyeonah Jung <gwi02378@gmail.com>
2021-04-26 22:13:08 +0900

    xml에 mainViewModel layout추가

--- app/src/main/java/com/example/movieappmvvm/comm/BaseActivity.kt
+++ app/src/main/java/com/example/movieappmvvm/comm/BaseActivity.kt
@@ -1,16 +1,23 @@
 package com.example.movieappmvvm.comm
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.movieappmvvm.R
 
-abstract class BaseActivity<R: BaseViewModel>: AppCompatActivity() {
-    abstract val viewModel: R
+abstract class BaseActivity<T: BaseViewModel>: AppCompatActivity() {
+    abstract val viewModel: T
     abstract val layoutResourceId: Int
     abstract fun initView()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layoutResourceId)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.navigationBarColor = ContextCompat.getColor(this, R.color.deepGray)
+        }
         initView()
     }
 }
