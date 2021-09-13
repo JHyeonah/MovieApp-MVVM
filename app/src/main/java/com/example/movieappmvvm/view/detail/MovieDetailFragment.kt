98b1575c12ecb862668d56ae0c3e36958d594eef
Hyeonah Jung <gwi02378@gmail.com>
2021-09-13 22:40:44 +0900

    detail fragment와 xml 연결

--- app/src/main/java/com/example/movieappmvvm/view/detail/MovieDetailFragment.kt
+++ app/src/main/java/com/example/movieappmvvm/view/detail/MovieDetailFragment.kt
@@ -1,6 +1,24 @@
 package com.example.movieappmvvm.view.detail
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.movieappmvvm.databinding.FragmentMovieDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MovieDetailFragment : Fragment() {
+    private lateinit var binding: FragmentMovieDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
 }
