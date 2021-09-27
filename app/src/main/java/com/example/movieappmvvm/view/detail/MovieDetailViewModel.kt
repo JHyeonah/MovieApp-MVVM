984b043092bda6a9515bde33da7291569dee52d0
Hyeonah Jung <gwi02378@gmail.com>
2021-09-27 22:11:15 +0900

    movie detail 호출 코드 작성

--- app/src/main/java/com/example/movieappmvvm/view/detail/MovieDetailViewModel.kt
+++ app/src/main/java/com/example/movieappmvvm/view/detail/MovieDetailViewModel.kt
@@ -1,10 +1,16 @@
 package com.example.movieappmvvm.view.detail
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.movieappmvvm.data.Movie
 import com.example.movieappmvvm.data.MovieRepository
 import com.example.movieappmvvm.util.Debug
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,9 +20,25 @@ class MovieDetailViewModel @Inject constructor(
 ): ViewModel() {
 
     private val movieId = savedStateHandle.get<Int>(MOVIE_ID)
+    val movie = MutableLiveData<Movie>()
 
     init {
+        getMovieDetail()
         Debug.log("movieId : $movieId")
+    }
+
+    private fun getMovieDetail() {
+        viewModelScope.launch {
+            val response = movieRepository.getMovieDetail(movieId ?: 0)
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    Debug.log("get movie detail : ${response.body()}")
+                    movie.postValue(response.body())
+                } else {
+                    Debug.log("get movie error detail: ${response.message()}")
+                }
+            }
+        }
     }
 
     companion object {
