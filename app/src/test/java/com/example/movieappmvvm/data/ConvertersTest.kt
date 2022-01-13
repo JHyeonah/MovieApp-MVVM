c6589566eaa4c7e49234c054db82c637142f2699
Hyeonah Jung <gwi02378@gmail.com>
2022-01-14 00:04:06 +0900

    converters 유닛 테스트 작성

--- app/src/test/java/com/example/movieappmvvm/data/ConvertersTest.kt
+++ app/src/test/java/com/example/movieappmvvm/data/ConvertersTest.kt
@@ -6,9 +6,13 @@ import org.junit.Test
 class ConvertersTest {
 
     private val list = arrayListOf("one", "two", "three")
-    private val json = "[ \"one\", \"two\", \"three\" ]"
+    private val json = "[\"one\",\"two\",\"three\"]"
 
     @Test fun stringToList() {
         assertEquals(list, MovieConverters().stringToList(json))
+    }
+
+    @Test fun listToJson() {
+        assertEquals(json, MovieConverters().listToJson(list))
     }
 }
